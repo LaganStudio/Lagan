79deba854b12eee71fe16e3a96e23cd9ded51303
iSosnitsky <spiritblossom@icloud.com>
2015-12-13 23:21:02 +0500

    New asset: Spikes

---
+++ Assets/Scripts/platforms/spikes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spikes : MonoBehaviour {
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D col){
+		IDamageable damageableObject = col.gameObject.GetComponent<IDamageable>();
+		if((damageableObject) != null){
+			damageableObject.getHit();
+		}
+	}
+}
